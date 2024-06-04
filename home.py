9dd61cca3ea4a52a17709140f9e12a10ccec5b7f
AVRSANAND <avrsanand2003@gmail.com>
2024-06-04 22:59:01 +0530

    Added Link to Github and LinkedIn

--- home.py
+++ home.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title("Welcome to mt personal portfolio")
 st.write("Will be adding all the python related projects in here :)")
+
+st.write("Check out my [GitHub](https://github.com/avrsanand)")
+st.write("Check out my [LinkedIn](https://www.linkedin.com/in/avrsanand/)")
+st.subheader("Python Simple Todo App")
+st.subheader("Zip Archive creator & extractor")
+
