3289bd57842bb35db7d34031221bdb2f566aa4aa
AVRSANAND <avrsanand2003@gmail.com>
2024-06-04 22:56:09 +0530

    Files creation

---
+++ contact.py
@@ -0,0 +1,5 @@
+import streamlit
+
+streamlit.title("Contact Me")
+
+streamlit.write("Email: avrsanand2003@gmail.com")
---
+++ home.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Welcome to mt personal portfolio")
+st.write("Will be adding all the python related projects in here :)")
