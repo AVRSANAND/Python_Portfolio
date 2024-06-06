94dd64eb05997cd071ecb52624606c789dc4e02e
AVRSANAND <avrsanand2003@gmail.com>
2024-06-06 11:49:45 +0530

    Create columns , image and description.

--- home.py
+++ home.py
@@ -1,10 +1,24 @@
 import streamlit as st
 
-st.title("Welcome to mt personal portfolio")
-st.write("Will be adding all the python related projects in here :)")
+st.set_page_config(layout="wide")
 
-st.write("Check out my [GitHub](https://github.com/avrsanand)")
-st.write("Check out my [LinkedIn](https://www.linkedin.com/in/avrsanand/)")
-st.subheader("Python Simple Todo App")
-st.subheader("Zip Archive creator & extractor")
+st.title("Welcome to my personal portfolio")
+# st.write("Will be adding all the python related projects in here :)")
 
+# st.write("Check out my [GitHub](https://github.com/avrsanand)")
+# st.write("Check out my [LinkedIn](https://www.linkedin.com/in/avrsanand/)")
+# st.subheader("Python Simple Todo App")
+# st.subheader("Zip Archive creator & extractor")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/avrsanand.jpg")
+
+with col2:
+    st.title("AVRS ANAND")
+    content = """
+    Hi, I am Anand! I am a Python programmer, and I love to build projects. 
+    I am currently learning and experimenting various things in python.  
+    """
+    st.info(content)
