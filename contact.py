062303d50e4fcf34cc55db2b5e89505ea9a7b7ff
AVRSANAND <avrsanand2003@gmail.com>
2024-06-07 23:13:35 +0530

    Add navigation menu

--- contact.py
+++
@@ -1,5 +0,0 @@
-import streamlit
-
-streamlit.title("Contact Me")
-
-streamlit.write("Email: avrsanand2003@gmail.com")
---
+++ pages/Contact.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Contact Me")
+
+st.write("Email: avrsanand2003@gmail.com")
+
+st.write("LinkedIn: [AVRSANAND](https://www.linkedin.com/in/avrsanand/)")
+
+st.write("GitHub: [AVRSANAND](https://github.com/AVRSANAND)")
