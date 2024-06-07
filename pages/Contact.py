20f2c032201d0f9036e47e6861b737c35b7d8300
AVRSANAND <avrsanand2003@gmail.com>
2024-06-07 23:57:29 +0530

    Contact Form added

--- pages/Contact.py
+++ pages/Contact.py
@@ -7,3 +7,10 @@ st.write("Email: avrsanand2003@gmail.com")
 st.write("LinkedIn: [AVRSANAND](https://www.linkedin.com/in/avrsanand/)")
 
 st.write("GitHub: [AVRSANAND](https://github.com/AVRSANAND)")
+
+with st.form(key="contact_form"):
+    user_email = st.text_input("Your email address")
+    user_message = st.text_area("Your message...")
+    submit_button = st.form_submit_button(label="Submit")
+    if submit_button:
+        st.write("Thank you !")
